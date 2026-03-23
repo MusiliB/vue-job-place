09170e5e7bf5b3d7313fe85c12278265714512b8
brian.musili <musilibrian07@gmail.com>
2026-03-23 16:49:55 +0300

    Computed in vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue';
 import Hero from '@/components/Hero.vue';
+import HomeCards from './components/HomeCards.vue';
+import JobListing from './components/JobListings.vue';
+
+
 </script>
 
 <template>
     <Navbar />
     <Hero />
+    <HomeCards/>
+    <JobListing :limit="3" :showButton="true"/>
 </template>
