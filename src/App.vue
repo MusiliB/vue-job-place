2beec3aba2f36b2d3f11bae23d87e89fdabbe269
brian.musili <musilibrian07@gmail.com>
2026-03-25 17:09:46 +0300

    Feat: implement routing

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue';
-import Hero from '@/components/Hero.vue';
-import HomeCards from './components/HomeCards.vue';
-import JobListing from './components/JobListings.vue';
+import { RouterView } from 'vue-router';
+
 
 
 </script>
 
 <template>
     <Navbar />
-    <Hero />
-    <HomeCards/>
-    <JobListing :limit="3" :showButton="true"/>
+    <RouterView />
 </template>
